756551caa206fb287f2d3ffbd10c51d0e8af3f4f
Helloar8 <c.helloar@escolar.ifrn.edu.br>
2025-09-25 09:45:01 -0300

    atividade cliente js de pokemon

---
+++ clientejs/script.js
@@ -0,0 +1,52 @@
+const listDiv = document.getElementById("pokemon-list");
+const detailsDiv = document.getElementById("pokemon-details");
+const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=100";
+
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+async function loadPokemonsList() {
+    try {
+        const res = await fetch(apiUrl);
+        if (!res.ok) throw new Error("Falha ao buscar a lista de Pokémons");
+        const data = await res.json();
+
+        listDiv.innerHTML = "";
+        data.results.forEach(pokemon => {
+            const pokemonItem = document.createElement("button");
+            pokemonItem.className = "btn btn-outline-primary w-100 mb-2 text-start pokemon-name";
+            pokemonItem.textContent = capitalizeFirstLetter(pokemon.name);
+            pokemonItem.onclick = () => loadPokemonDetails(pokemon.url);
+            listDiv.appendChild(pokemonItem);
+        });
+    } catch (error) {
+        console.error("Erro ao carregar a lista de Pokémons:", error);
+        listDiv.innerHTML = "<p class='text-danger'>Falha ao carregar a lista de Pokémons. Tente novamente mais tarde.</p>";
+    }
+}
+
+async function loadPokemonDetails(url) {
+    try {
+        const res = await fetch(url);
+        if (!res.ok) throw new Error("Falha ao buscar os detalhes do Pokémon");
+        const data = await res.json();
+
+        detailsDiv.innerHTML = `
+            <h2 class="text-primary fs-1">${capitalizeFirstLetter(data.name)}</h2>
+            <img src="${data.sprites.front_default}" alt="${data.name}" class="img-fluid mb-3" style="width: 200px; height: 200px;">
+            <p class= "fs-4"><strong>Altura:</strong> ${data.height / 10} m</p>
+            <p class= "fs-4"><strong>Peso:</strong> ${data.weight / 10} kg</p>
+            <h3 class = "fs-3">Habilidades:</h3>
+            <ul class="list-group">
+                ${data.abilities.map(ability => `<li class="list-group-item">${capitalizeFirstLetter(ability.ability.name)}</li>`).join('')}
+            </ul>
+        `;
+    } catch (error) {
+        console.error("Erro ao carregar os detalhes do Pokémon:", error);
+        detailsDiv.innerHTML = "<p class='text-danger'>Falha ao carregar os detalhes do Pokémon. Tente novamente mais tarde.</p>";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadPokemonsList);
